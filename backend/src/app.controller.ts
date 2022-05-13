fc040f7bc575d6604966dc3fb423f3c804e45198
CutieCat6778 <thinhnguyenhuuhung@gmail.com>
2022-05-13 23:19:03 +0200

    fixed how the server return

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,9 +1,12 @@
-import { Controller, Get, HttpStatus } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 
 @Controller('')
 export class AppController {
   @Get('/')
   returnOk() {
-    return HttpStatus.OK;
+    return {
+      status: 200,
+      message: 'Hello world',
+    };
   }
 }
