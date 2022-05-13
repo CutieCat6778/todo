7b17adb3442d36b71d6631b350378a80f4622f71
CutieCat6778 <thinhnguyenhuuhung@gmail.com>
2022-05-13 16:10:15 +0200

    succesfully added login and signup function

--- frontend/pages/signup.tsx
+++ frontend/pages/signup.tsx
@@ -1,5 +1,5 @@
 import { gql } from "@apollo/client";
-import { Box, Button, Flex, FormControl, FormErrorMessage, FormHelperText, FormLabel, Heading, Input } from "@chakra-ui/react";
+import { Box, Button, Flex, FormControl, FormErrorMessage, FormHelperText, FormLabel, Heading, Input, Text } from "@chakra-ui/react";
 import request from "graphql-request";
 import { NextPage } from "next";
 import Link from "next/link";
@@ -10,24 +10,22 @@ export interface IsError {
   message: string | null;
 }
 
-const GET_USER = gql`
-  mutation login(
-    $input: LoginUserInput!
+const SIGNUP_USER = gql`
+  mutation signup(
+    $input: SignupUserInput!
   ) {
-    login(loginUserInput: $input) {
-      user {
-        username
-        _id
-      } 
-      access_token
+    signup(signupUserInput: $input) {
+      username
+      _id
     }
   }
 `
 
-const Login: NextPage<any> = ({ auth }) => {
+const Signup: NextPage<any> = ({ auth }) => {
 
   const [username, setUsername] = useState<string>('');
   const [password, setPassword] = useState<string>('');
+  const [email, setEmail] = useState<string>('');
   const [isError, setIsError] = useState<IsError>({
     isError: false,
     message: null
@@ -41,17 +39,38 @@ const Login: NextPage<any> = ({ auth }) => {
     setPassword(e.target.value);
   }
 
-  async function logIn() {
-    // eslint-disable-next-line react-hooks/rules-of-hooks
+  function handleInputChangeEmail(e: React.ChangeEvent<HTMLInputElement>) {
+    setEmail(e.target.value);
+  }
+
+  async function Signup() {
+    if(password.length < 6) return setIsError({
+      isError: true,
+      message: "Password is too, it need to be longer then 6 character!",
+    })
+
+    const regex = new RegExp(/^[A-Za-z]+$/);
+    if(!regex.test(username)) return setIsError({
+      isError: true,
+      message: "Username shouldn't contain special characters!",
+    })
+
+    const regexEmail = new RegExp(/(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/)
+    if(!regexEmail.test(email)) return setIsError({
+      isError: true,
+      message: "Invalid email format!",
+    })
+
     try {
-      const { login } = await request("http://localhost:6969/graphql/", GET_USER, {
+      const { signup } = await request("http://localhost:6969/graphql/", SIGNUP_USER, {
         input: {
           username,
           password,
+          email
         }
       })
-      if (login) {
-        console.log(login);
+      if (signup) {
+        console.log(signup);
         setIsError({
           isError: false,
           message: null,
@@ -73,16 +92,16 @@ const Login: NextPage<any> = ({ auth }) => {
       <Flex alignItems={"center"}>
         <Box width={"18rem"}>
           <Heading ml="1rem">
-            Login to your Todo
+            Create a account with Todo
           </Heading>
           <Flex width={"12rem"} justifyContent="space-around" m="1rem">
-            <Link href="/signup" passHref>
+            <Link href="/login" passHref>
               <Button colorScheme={"blue"}>
-                Sign Up
+                Log In
               </Button>
             </Link>
-            <Button colorScheme={"green"} onClick={logIn}>
-              Log In
+            <Button colorScheme={"green"} onClick={Signup}>
+              Sign Up
             </Button>
           </Flex>
         </Box>
@@ -93,14 +112,19 @@ const Login: NextPage<any> = ({ auth }) => {
             <FormHelperText>Enter your username, not your username</FormHelperText>
           </Box>
           <Box mt="1rem">
+            <FormLabel htmlFor="username">Email</FormLabel>
+            <Input id="email" type="email" value={email} onChange={handleInputChangeEmail} placeholder="Your email" />
+            <FormHelperText>Enter your email, not your gmail</FormHelperText>
+          </Box>
+          <Box mt="1rem">
             <FormLabel htmlFor="username">Password</FormLabel>
             <Input id="password" type="password" value={password} onChange={handleInputChangePassword} placeholder="Your password" />
-            <FormHelperText>Enter your password, not your bank PIN ;)</FormHelperText>
+            <FormHelperText>It needs to be longer then 6 character</FormHelperText>
           </Box>
           {
             isError.isError === false ? (
               <FormHelperText>
-                Please login with your username and password ;)
+                If there are some error show up, contact me: <Text textDecoration={"underline"} display="inline" fontWeight={600}><Link href="mailto:support@thinh.tech">support@thinh.tech</Link></Text>
               </FormHelperText>
             ) :
               (
@@ -115,7 +139,7 @@ const Login: NextPage<any> = ({ auth }) => {
   )
 }
 
-export default Login
+export default Signup
 
 export async function getStaticProps() {
   return {
